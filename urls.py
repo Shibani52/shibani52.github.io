d1e68e07671b191d6508cb0bed4e4e19b9a63a11
Shibani52 <169175789+Shibani52@users.noreply.github.com>
2024-05-23 11:41:05 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,34 +1,22 @@
-from django.urls import path
-from . import views
-from django.contrib.auth.views import LoginView,LogoutView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
-from .forms import UserLoginForm
-from django.conf.urls.static import static
+"""tourAndTravel URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 
 urlpatterns = [
-    path('', views.IndexView, name="home"),
-    path('accounts/login/',LoginView.as_view(authentication_form=UserLoginForm),name="login_url"),
-    path('register/',views.registerView,name="register_url"),
-    path('logout/',LogoutView.as_view(next_page='home'),name="logout"),
-    path('package/',views.PackageView,name="package"),
-    path('flights/',views.FlightView,name="flights"),
-    path('hotels/',views.HotelView,name="hotels"),
-    path('places/',views.PlacesView,name="places"),
-    path('bookflight/<str:flight_num>/<str:date>',views.Flightbook,name="bookflight"),
-    path('userflight/<str:flight_num>/<str:date>/<int:seat>',views.FlightSubmit,name='userflight'),
-    path('bookhotel/<str:hotel>/<str:date>',views.Hotelbook,name="bookflight"),
-    path('userhotel/<str:hotel>/<str:date>/<int:room>',views.HotelSubmit,name='userflight'),
-    path('bookpackage/<str:source>/<str:city>/<str:date>',views.PackageBook,name="bookpackage"),
-    path('userpackage/<str:flight>/<str:hotel>/<str:date>/<int:room>/<int:seat>',views.PackageSubmit,name='userpackage'),
-    path('accounts/profile/',views.Dashboard,name='dashboard'),
-    path('cancelflight/<str:flight>/<str:date>/<int:seat>',views.CancelFlight,name='CancelFlight'),
-    path('concanflight/<str:flight>/<str:date>/<int:seat>',views.ConfirmCancelFlight,name='ConfirmCancelFlight'),
-    path('cancelhotel/<str:hotel>/<str:date>/<int:room>',views.CancelHotel,name='CancelHotel'),
-    path('concanhotel/<str:hotel>/<str:date>/<int:room>',views.ConfirmCancelHotel,name='ConfirmCancelHotel'),
-    path('cancelpackage/<str:flight>/<int:seat>/<str:hotel>/<str:date>/<int:room>',views.CancelPackage,name='CancelPackage'),
-    path('concanpackage/<str:flight>/<int:seat>/<str:hotel>/<str:date>/<int:room>',views.ConfirmCancelPackage,name='ConfirmCancelPackage'),
-    ]
-
-urlpatterns+= staticfiles_urlpatterns()
-urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('travelapp.urls')),
+]
